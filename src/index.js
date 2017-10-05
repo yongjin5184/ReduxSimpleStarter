f705f9b17d88081244c3bb4de31584a9a5f9a8d8
Kimyongjin <yongjin5184@gmail.com>
2017-10-05 21:01:32 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+  return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+ReactDOM.render(<App />, document.querySelector('.container'));
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
